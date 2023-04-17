54abf5ecfaca5dbf08e7b8b8725909ec3016a39e
Andrew Golovnev <andrey.golovnev71@yandex.ru>
2023-04-17 22:54:37 +0300

    Updated premake and gitignore

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,7 @@
 project "ImGui"
     kind "StaticLib"
     language "C++"
+	staticruntime "off"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -21,9 +22,23 @@ project "ImGui"
     }
 
     filter "system:windows"
-        systemversion "latest"
-        cppdialect "C++17"
-        staticruntime "On"
+		systemversion "latest"
+		cppdialect "C++17"
 
-    filter { "system:windows", "configurations:Release" }
-        buildoptions "/MT"
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
